56f138251d6b6ec0c0a9602406b9233731cc96d6
Anatoly Shikolay <eduard_dum@mail.com>
2014-12-19 01:14:45 -0500

    Add hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT | 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(port, function () {
+    console.log('Vizzit app listening at port:%s', port)
+});
