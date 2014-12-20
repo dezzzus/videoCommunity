69ca01ef28c340d595803b58a44b14547511039f
Anatoly Shikolay <eduard_dum@mail.com>
2014-12-20 00:22:38 -0500

    Add tour creation interface

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var bodyParser = require('body-parser');
+var MongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
 
 var app = express();
-var port = process.env.PORT | 3000;
+app.collection = {};
+app.use(bodyParser.urlencoded({'extended': true}));
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +14,48 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.listen(port, function () {
-    console.log('Vizzit app listening at port:%s', port)
+app.get('/tour', function (req, res) {
+    app.collection.tour.find({}).toArray(
+        function (err, tours) {
+            res.render('tour', {
+                tours: tours
+            });
+        }
+    );
 });
+
+app.post('/tour', function (req, res) {
+    var videoUrl = req.body['videoUrl'];
+    var newTour = {
+        videoUrl: videoUrl
+    };
+    app.collection.tour.insert(newTour, function (err, dbTour) {
+        if (err) {
+            console.log('DB err' + err);
+        }
+    });
+    res.redirect('/tour');
+});
+
+app.get('/tour/:tid', function (req, res) {
+    var tid = req.param('tid');
+    app.collection.tour.findOne({'_id': ObjectID(tid)}, function (err, tour) {
+        res.render('tour_details', {
+            tour: tour
+        });
+    });
+});
+
+var mongoURI = 'mongodb://vizzit123:321tizziv@proximus.modulusmongo.net:27017/i8Jypyzy';
+var port = process.env.PORT || 3000;
+
+MongoClient.connect(mongoURI, function (dbErr, db) {
+    if (dbErr) throw dbErr;
+
+    app.collection.tour = db.collection('tour');
+
+    app.listen(port, function () {
+        console.log('Vizzit app listening at port:%s', port)
+    });
+});
+
