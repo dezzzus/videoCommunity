a03c6ff0c8efa73c04b9e4a1e10c5de1e414bd56
Ilya <ia1717@gmail.com>
2015-01-04 11:37:40 -0500

    tour edit, delete confirmation, a little bit of code dup reduction

---
+++ static/js/vv_misc_ui.js
@@ -0,0 +1,26 @@
+/**
+ * For now, a single place for all VV JS UI.
+ * Will likely split in the future, but this makes it simpler to manage now
+ * (better than embedded in html, here it can be jshinted).
+ */
+
+/**
+ * Global namespace for easier access
+ */
+var vvzzt = vvzzt || {};
+vvzzt.ui = vvzzt.ui || {};
+
+/**
+ * Simple blocking prompt for now, use lightbox in the future:
+ */
+vvzzt.ui.confirmThenProceed = function(msg, func) {
+	if (confirm(msg)) {
+		func();
+	}
+};
+
+vvzzt.ui.followURLAfterConfirm = function(msg, url) {
+	vvzzt.ui.confirmThenProceed(msg, function() {
+		window.location.href = url;
+	});
+};
